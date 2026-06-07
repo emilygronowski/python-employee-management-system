b9239be0677df25e54886c69535c14aafe54588f
Your Name <manditalaskar123@gmail.com>
2026-06-08 01:04:24 +0530

    feat: add Employee class and sample records to main.py

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
-from scripts import sum
+class Employee:
+    def __init__(self, name, id_number, department, job_title):
+        self.name = name
+        self.id_number = id_number
+        self.department = department
+        self.job_title = job_title
 
+    def display_employee(self):
+        print(f"Name: {self.name}, ID: {self.id_number}, Department: {self.department}, Job Title: {self.job_title}")
 
-def main():
-    print("Hello World")
-    print(sum(10, 10))
+# Create the objects
+emp1 = Employee("Susan Meyers", 47899, "Accounting", "Vice President")
+emp2 = Employee("Mark Jones", 39119, "IT", "Programmer")
+emp3 = Employee("Joy Rodgers", 81774, "Manufacturing", "Engineer")
 
-
-main()
+# Display the data
+emp1.display_employee()
+emp2.display_employee()
+emp3.display_employee()
