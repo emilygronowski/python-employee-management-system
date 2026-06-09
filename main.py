48d5f12a32fbfc53fa44db357d3a9c5e02954efa
Elijah Brathwaite <ebrathwaite06@gmail.com>
2026-06-08 23:06:09 -0400

    Scoped back down to deliverables in #4

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import employee_manager as em
+
 
 
 def main():
@@ -7,49 +7,27 @@ def main():
         print("\nEmployee Management System")
         print("1. Look up an employee")
         print("2. Add new employee")
-        print("3. Edit Employee")
-        print("4. Delete Employee")
+        print("3. Edit employee")
+        print("4. Delete employee")
         print("5. Exit")
 
         choice = input("Enter your choice: ")
 
         #Look up an employee
-        if choice == "1":
-            emp_id = int(input("Enter ID to look up: "))
-            employee = em.get_employee(emp_id)
-            print(employee if employee else "Employee not found")
-
-        #add an employee
+        if choice == '1':
+            pass
         elif choice == '2':
-            emp_id = int(input("Enter ID: "))
-            name = input("Enter Name: ")
-            dept = input("Enter Department: ")
-            title = input("Enter Job Title: ")
-            em.add_employee(emp_id, name, dept, title)
-            print("Employee Created")
-
-        #edit an employee
+            pass
         elif choice == '3':
-            emp_id = int(input("Enter Employee ID to edit: "))
-            name = input("Enter new Name (or leave blank): ")
-            dept = input("Enter new Dept (or leave blank): ")
-            title = input("Enter new Title (or leave blank): ")
-            success = em.edit_employee(emp_id, name or None, dept or None, title or None)
-            print("Updated successfully." if success else "Error: ID not found.")
-
-        #delete an employee
+            pass
         elif choice == '4':
-            emp_id = int(input("Enter Employee ID to delete: "))
-            success = em.delete_employee(emp_id)
-            print("Employee deleted!" if success else "Error: ID not found.")
-
-        #exit
+            pass
         elif choice == '5':
-            print("Exiting program...")
-            break #break menu loop
+            print("Exiting the program. Goodbye!")
+            break
+        else:
+            print("ERROR: Please enter a valid menu option.")
 
-        else: #invalid input
-            print("Erros: Please enter one of the values shown")
 
 if __name__ == "__main__":
     main()
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,5 @@
 import pytest
-
-
+from unittest.mock import patch
 from main import main
 
 
@@ -9,7 +8,27 @@ class TestMain:
     def _capsys(self, capsys):
         self.capsys = capsys
 
-    def test_main(self):
-        main()
-        captured = self.capsys.readouterr()
-        assert captured.out == "Hello World\n20\n"
+    def test_main_exit(self):
+        #test to input to return '5' to exit the program
+        with patch('builtins.input', return_value='5'):
+            main()
+            captured = self.capsys.readouterr()
+
+            #verification that menu options get printed
+            assert "1. Look up an employee" in captured.out
+            assert "5. Exit" in captured.out
+
+    def test_invalid_input(self):
+        #test to input an invalid menu option
+        with patch('builtins.input', side_effect=['9', '5']): #return '9' first to trigger error message, then '5' to exit the program
+            main()
+            captured = self.capsys.readouterr()
+
+            #verification that error message gets printed
+            assert "ERROR: Please enter a valid menu option." in captured.out
+    
+    #tests for options 1,2,3, and 4
+    @pytest.mark.parametrize("choice", ['1', '2', '3', '4'])
+    def test_menuUoptions(self, choice):
+        with patch('builtins.input', side_effect=[choice, '5']):
+            main()
