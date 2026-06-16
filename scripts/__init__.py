7dfac8070c337a053fab6fc463a33cc54052457f
Emily <emilygronowski3@gmail.com>
2026-06-16 01:38:52 +0000

    Adds Anti-collision research implementation

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -1,5 +1,10 @@
+from scripts.employee import Employee
+from scripts.employee_id_anticollision import adding_employee, find_available_id
 from scripts.example import sum
 
 __all__ = (
+    'Employee',
+    'adding_employee',
+    'find_available_id',
     'sum'
 )
